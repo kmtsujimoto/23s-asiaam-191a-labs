09657fa879e93ff5f4cd70a380663802c42a7de9
kmtsujimoto <kyle.tsujimoto@gmail.com>
2023-05-18 02:50:55 -0700

    week7lab5

--- week06/js/init.js
+++ week06/js/init.js
@@ -1,5 +1,5 @@
 // declare variables
-let mapOptions = {'center': [34.0709,-118.444],'zoom':5}
+let mapOptions = {'center': [34.0709,-118.444],'zoom':10}
 
 // use the variables
 const map = L.map('the_map').setView(mapOptions.center, mapOptions.zoom);
@@ -11,12 +11,49 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 function addMarker(data){
     // console.log(data)
     // these are the names of our lat/long fields in the google sheets:
-    L.marker([data.lat,data.lng]).addTo(map).bindPopup(`<h2>${data['Where did you get vaccinated?']}</h2> <h3>${data['Have you been vaccinated?']}</h3>`)
-    createButtons(data.lat,data.lng,data['Where did you get vaccinated?'])
-    return
+    if(data["What is your highest level of educational attainment?"] == "Bachelor's Degree (B.S., B.A., etc.)") {
+        L.circleMarker([data.lat,data.lng],{
+            radius: 15,
+            color: 'green'
+        }).addTo(map).bindPopup(`<h2>${data['What is your hometown?']}</h2>`)
+        createButtons(data.lat,data.lng,data['What is your hometown?'],'green')
+        return
+    }
+    else if(data["What is your highest level of educational attainment?"] == "No High School Degree") {
+        L.circleMarker([data.lat,data.lng],{
+            radius: 15,
+            color: 'red'
+        }).addTo(map).bindPopup(`<h2>${data['What is your hometown?']}</h2>`)
+        createButtons(data.lat,data.lng,data['What is your hometown?'],'red')
+        return
+    }
+    else if(data["What is your highest level of educational attainment?"] == "High School Degree") {
+        L.circleMarker([data.lat,data.lng],{
+            radius: 15,
+            color: 'yellow'
+        }).addTo(map).bindPopup(`<h2>${data['What is your hometown?']}</h2>`)
+        createButtons(data.lat,data.lng,data['What is your hometown?'],'yellow')
+        return
+    }
+    else if(data["What is your highest level of educational attainment?"] == "Graduate Degree") {
+        L.circleMarker([data.lat,data.lng],{
+            radius: 15,
+            color: 'blue'
+        }).addTo(map).bindPopup(`<h2>${data['What is your hometown?']}</h2>`)
+        createButtons(data.lat,data.lng,data['What is your hometown?'],'blue')
+        return
+    }
+    else if(data["What is your highest level of educational attainment?"] == "PhD or above") {
+        L.circleMarker([data.lat,data.lng],{
+            radius: 15,
+            color: 'purple'
+        }).addTo(map).bindPopup(`<h2>${data['What is your hometown?']}</h2>`)
+        createButtons(data.lat,data.lng,data['What is your hometown?'],'purple')
+        return
+    }
 }
 
-function createButtons(lat,lng,title){
+function createButtons(lat,lng,title,color){
     const newButton = document.createElement("button"); // adds a new button
     newButton.id = "button"+title; // gives the button a unique id
     newButton.innerHTML = title; // gives the button a title
@@ -25,11 +62,39 @@ function createButtons(lat,lng,title){
     newButton.addEventListener('click', function(){
         map.flyTo([lat,lng]); //this is the flyTo from Leaflet
     })
-    const spaceForButtons = document.getElementById('placeForButtons')
-    spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    if(color=='red'){
+        newButton.style.color='red';
+        newButton.style.background='white';
+        const spaceForButtons = document.getElementById('placeForRedButtons');
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    }
+    else if(color=='green'){
+        newButton.style.color='green';
+        newButton.style.background='white';
+        const spaceForButtons = document.getElementById('placeForGreenButtons')
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    }
+    else if(color=='blue'){
+        newButton.style.color='blue';
+        newButton.style.background='white';
+        const spaceForButtons = document.getElementById('placeForBlueButtons')
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    }
+    else if(color=='purple'){
+        newButton.style.color='purple';
+        newButton.style.background='white';
+        const spaceForButtons = document.getElementById('placeForPurpleButtons')
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    }
+    else if(color=='yellow'){
+        newButton.style.color='gold';
+        newButton.style.background='white';
+        const spaceForButtons = document.getElementById('placeForYellowButtons')
+        spaceForButtons.appendChild(newButton);//this adds the button to our page.
+    }
 }
 
-const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSNq8_prhrSwK3CnY2pPptqMyGvc23Ckc5MCuGMMKljW-dDy6yq6j7XAT4m6GG69CISbD6kfBF0-ypS/pub?output=csv"
+const dataUrl = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR76ajamkW33LOYWPvm8LSLIVeD3td1RYaT_FfiYx2t0sVdPuob-rRLpJ3daOd2J5Aq3AAUdGcv9rg1/pub?output=csv"
 
 function loadData(url){
     Papa.parse(url, {
